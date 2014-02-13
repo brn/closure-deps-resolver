61f8c36531e6e19a1e628a020e4e8c459fd82ad8
brn <dobaw20@gmail.com>
2014-02-13 18:50:50 +0900

    fix test.

--- test/noassertion-test.js
+++ test/noassertion-test.js
@@ -13,15 +13,5 @@ var closureDepsResolver = new Resolver({
       onMemoryCache : true
     });
 
-var amdDepsResolver = new Resolver({
-      root : ['/Users/aono_taketoshi/Documents/workspace/adsys_ads_mng_trunk/WebContent/js'],
-      pattern : amdPattern,
-      onMemoryCache : true
-    });
-
 var moduleMap = closureDepsResolver.resolveSync(true);
 console.log(Object.keys(moduleMap));
-
-/*amdDepsResolver.resolve().then(function(modules) {
-  console.log(modules);
-});*/
