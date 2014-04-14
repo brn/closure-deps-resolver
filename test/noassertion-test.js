0c8b138090cb7bee07c40309e90d096b02dc41ff
brn <dobaw20@gmail.com>
2014-04-14 10:57:56 +0900

    add getRequiredModules method to Module.

--- test/noassertion-test.js
+++ test/noassertion-test.js
@@ -9,9 +9,11 @@ var Resolver = Deps.Resolver;
 var amdPattern = Deps.amdPattern;
 
 var closureDepsResolver = new Resolver({
-      root : '../node_modules/closure-library',
+      root : './node_modules/closure-library',
       onMemoryCache : true
     });
 
 var moduleMap = closureDepsResolver.resolveSync(true);
-console.log(Object.keys(moduleMap));
+for (var prop in moduleMap) {
+  console.log(moduleMap[prop].getRequiredModules());
+}
