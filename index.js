39ea537d98361064c88a2ca2b704863e43e59a9c
brn <dobaw20@gmail.com>
2014-10-22 10:42:47 +0900

    fix cache management.

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 /**
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2013 Taketoshi Aono(brn)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  * @fileoverview
  * @author Toketoshi Aono
  */
@@ -25,6 +46,7 @@ var amdPattern = require('./lib/patterns/amd-pattern');
 var Pattern = require('./lib/pattern');
 var BASE_REG = /goog\/base.js$/;
 var pathutil = require('./lib/pathutil');
+var CacheController = require('./lib/cache-controller');
 
 
 /**
@@ -175,6 +197,14 @@ Object.defineProperties(ClosureDepsResolver.prototype, {
     },
     configurable : true,
     enumerable : false
+  },
+
+  cacheController: {
+    get: function() {
+      return new CacheController(this._depsCache);
+    },
+    configurable: true,
+    enumerable: true
   }
 });
 
